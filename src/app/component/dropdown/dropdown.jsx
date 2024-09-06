2d92e0ee2931789d495e215b13d93021ec4336b2
SoniBasant <sbasant.jnct@gmail.com>
2024-09-06 14:57:52 +0530

    Make dropwoen item width full

--- src/app/component/dropdown/dropdown.jsx
+++ src/app/component/dropdown/dropdown.jsx
@@ -40,8 +40,8 @@ export default function Dropdown() {
 
 const menuStyle = {
   dropdownContainer: 'max-w-[181px] relative inline-block text-left shadow',
-  downIcon: '-mr-1 h-5 w-5 text-gray-400',
-  menuItemStyle: 'flex flex-left px-4 py-2 text-belowSize text-VeryLightGrayLightModeBackground dark:text-DarkGrayLightModeInput data-[focus]:bg-gray-100 data-[focus]:text-gray-900',
-  menuItemsContainerStyle: 'absolute right-0 z-10 mt-1 w-full origin-top-right rounded-md bg-DarkBlueDarkModeElements dark:bg-WhiteDarkModeTextnLightModeElements shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in',
   menuButtonStyle: 'inline-flex justify-left gap-x-8 rounded-md bg-DarkBlueDarkModeElements dark:bg-WhiteDarkModeTextnLightModeElements px-6 py-3 text-belowSize text-VeryLightGrayLightModeBackground dark:text-DarkGrayLightModeInput shadow-sm ring-inset ring-gray-300',
+  downIcon: '-mr-1 h-5 w-5 text-gray-400',
+  menuItemsContainerStyle: 'absolute right-0 z-10 mt-1 w-full origin-top-right rounded-md bg-DarkBlueDarkModeElements dark:bg-WhiteDarkModeTextnLightModeElements shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in',
+  menuItemStyle: 'w-full flex flex-left px-4 py-2 text-belowSize text-VeryLightGrayLightModeBackground dark:text-DarkGrayLightModeInput data-[focus]:bg-gray-100 data-[focus]:text-gray-900',
 }
